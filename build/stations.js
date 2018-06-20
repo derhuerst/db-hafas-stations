915611d42a1b0778b522f12df610fe9e3d1e9316
Jannis R <mail@jannisr.de>
2018-06-20 13:23:10 +0200

    throttle all HAFAS calls, fix build :bug:

--- build/stations.js
+++ build/stations.js
@@ -1,18 +1,21 @@
 'use strict'
 
-const walk = require('discover-db-stations')
+const createWalk = require('hafas-discover-stations')
+const throttle = require('db-hafas/throttle')
+const createEstimate = require('hafas-estimate-station-weight')
+const weights = require('compute-db-station-weight/lib/weights')
 const concurrentThrough = require('through2-concurrent')
 const progressStream = require('progress-stream')
 const pump = require('pump')
 
-const estimateStationWeight = require('./estimate-station-weight')
+const throttledHafas = throttle(10, 1000) // 10 reqs/s
+const walk = createWalk(throttledHafas)
 
 const maxIterations = 30
 const weight0Msg = `\
 has a weight of 0. Probably there are no departures here.`
 
-const berlinHbf = '8011160'
-
+const estimateStationWeight = createEstimate(throttledHafas, weights)
 const computeWeight = (s, _, cb) => {
 	estimateStationWeight(s.id + '', maxIterations)
 	.then(weight => {
@@ -23,22 +26,23 @@ const computeWeight = (s, _, cb) => {
 	.catch((err) => {
 		if (err.isHafasError) {
 			console.error(s.id + '', s.name, err.message || (err + ''))
-			cb()
+			cb(null)
 		} else cb(err)
 	})
 }
 
+const berlinHbf = '8011160'
+
 const download = () => {
 	const data = walk(berlinHbf)
 	const weight = concurrentThrough.obj({maxConcurrency: 5}, computeWeight)
-	weight.on('data', (s) => console.log(s.id, s.weight))
 	const progess = progressStream({objectMode: true})
 
 	data.on('stats', ({requests, stations, edges, queued}) => {
 		progess.setLength(stations + queued)
 	})
 
-	return pump(data, weight, progess)
+	return pump(data, weight, progess, (err) => {})
 }
 
 module.exports = download
