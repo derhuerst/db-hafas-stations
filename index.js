a20f97033b270f0e7645c62d41e1b891c2a1ab20
Jannis R <mail@jannisr.de>
2018-06-24 13:17:37 +0200

    unpack data.ndjson, full reader

--- index.js
+++ index.js
@@ -3,12 +3,30 @@
 const path = require('path')
 const fs = require('fs')
 const ndjson = require('ndjson')
+const through = require('through2')
 
-const src = path.join(__dirname, 'data.ndjson')
+const srcSparse = path.join(__dirname, 'data.ndjson')
+const srcFull = path.join(__dirname, 'full.ndjson')
 
-const readStations = () => {
-	return fs.createReadStream(src)
+const unpackSparse = (s, _, cb) => {
+	cb(null, {
+		type: 'station',
+		id: s[0],
+		name: s[1],
+		weight: s[2]
+	})
+}
+
+const readSparse = () => {
+	return fs.createReadStream(srcSparse)
+	.pipe(ndjson.parse())
+	.pipe(through.obj(unpackSparse))
+}
+
+const readFull = () => {
+	return fs.createReadStream(srcFull)
 	.pipe(ndjson.parse())
 }
 
-module.exports = readStations
+readSparse.full = readFull
+module.exports = readSparse
