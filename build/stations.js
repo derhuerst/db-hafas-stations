7e40e1f2d0bf40c96e731310cae65fd044608672
Jannis R <mail@jannisr.de>
2019-10-28 15:10:35 +0100

    db-hafas@3 -> hafas-client@4, Node 8+ :boom:

--- build/stations.js
+++ build/stations.js
@@ -1,15 +1,19 @@
 'use strict'
 
+const withThrottling = require('hafas-client/throttle')
+const createHafas = require('hafas-client')
+const dbProfile = require('hafas-client/p/db')
 const createWalk = require('hafas-discover-stations')
-const createThrottledHafas = require('db-hafas/throttle')
 const createEstimate = require('hafas-estimate-station-weight')
 const weights = require('compute-db-station-weight/lib/weights')
 const concurrentThrough = require('through2-concurrent')
 const progressStream = require('progress-stream')
 const pump = require('pump')
 
-const throttledHafas = createThrottledHafas('db-hafas-stations build', 10, 1000) // 10 reqs/s
-const walk = createWalk(throttledHafas)
+const userAgent = 'db-hafas-stations build'
+const createThrottledHafas = withThrottling(createHafas, 10, 1000) // 10 reqs/s)
+const hafas = createThrottledHafas(dbProfile, userAgent)
+const walk = createWalk(hafas)
 
 const minute = 60 * 1000
 
@@ -24,7 +28,7 @@ const maxIterations = 30
 const weight0Msg = `\
 has a weight of 0. Probably there are no departures here. Using weight 1.`
 
-const estimateStationWeight = createEstimate(throttledHafas, weights)
+const estimateStationWeight = createEstimate(hafas, weights)
 const computeWeight = (s, _, cb) => {
 	estimateStationWeight(s.id, maxIterations)
 	.then(weight => {
